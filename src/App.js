a37c09b267326d9a00e088addd8309c18dc0ea20
terenceyin <tyin05@student.ubc.ca>
2024-08-30 17:36:15 -0700

    changed Logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import yinYang from './images/yy.svg'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={yinYang} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
