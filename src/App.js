1139125b952fc2be7e9384a868f26df653640715
terenceyin <tyin05@student.ubc.ca>
2024-08-30 18:11:26 -0700

    added navbar component

--- src/App.js
+++ src/App.js
@@ -8,16 +8,9 @@ function App() {
       <header className="App-header">
         <img src={yinYang} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is Terence Yin's Personal Website. It is currently under construction.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Stack from '@mui/material/Stack';
+
+
+function NavBar() {
+  return (
+    <>
+        <Stack spacing={2}>
+            <Item>Item 1</Item>
+            <Item>Item 2</Item>
+            <Item>Item 3</Item>
+        </Stack>
+    
+    </>
+  )
+}
+
+export default NavBar
