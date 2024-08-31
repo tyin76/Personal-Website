9d191dc9b3a3c2ece85c55fedf3775d3a77ff4f9
terenceyin <tyin05@student.ubc.ca>
2024-08-30 20:00:41 -0700

    changed more navbar and got custom TY logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,36 +3,43 @@ import Stack from '@mui/material/Stack';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
 import '../styles/Fonts.css'
-import logo from '../images/yy.svg'
+import yy from './Logo.js';
+import '../styles/NavBar.css'
+import Logo from '../components/Logo.js'
 
+
+export function CustomNavButton({navText}) {
+  return (
+    <Button variant='text' sx={{
+    color: '#86C232',
+    fontFamily: 'Roboto-BoldItalic',
+    fontSize: '20px',
+    '&:hover' : {
+      backgroundColor: '#474B4F'
+    }
+  }}>{navText}</Button>
+  )
+}
 
 function NavBar() {
 
-  function CustomNavButton({navText}) {
-      return (
-        <Button variant='text' sx={{
-        color: '#86C232',
-        fontFamily: 'Roboto-BoldItalic',
-        fontSize: '20px',
-        '&:hover' : {
-          backgroundColor: '#474B4F'
-        }
-      }}>{navText}</Button>
-      )
-  }
+  
 
   return (
     <>
+    <nav>
+    <Logo width={'150px'} height={'150px'} className='navbar-logo'></Logo>
         <Stack spacing={3} direction={'row'} sx={{
           p : 1,
-          justifyContent: 'center'
+          justifyContent: 'center',
           }}>
             <CustomNavButton navText={'About'}></CustomNavButton>
             <CustomNavButton navText={'Experience'}></CustomNavButton>
             <CustomNavButton navText={'Skills'}></CustomNavButton>
             <CustomNavButton navText={'Projects'}></CustomNavButton>
         </Stack>
-    
+
+    </nav>
     </>
   )
 }
